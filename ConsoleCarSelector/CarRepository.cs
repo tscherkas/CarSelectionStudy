f4c58db254eca0ea01c6ba700e0ab8388bc8659a
icharkas <icharkas@servicechannel.com>
2021-04-04 19:53:15 +0300

    Add CarRepository and CarRepositoryBrowser tests.

---
+++ ConsoleCarSelector/CarRepository.cs
@@ -0,0 +1,26 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleCarSelector
+{
+    public class CarRepository
+    {
+        private string root_url;
+
+        public CarRepository(string root_url)
+        {
+            this.root_url = root_url;
+        }
+
+        public IEnumerable<CarListItem> GetCarsByPrice(int priceFrom, int priceTo, string currency)
+        {
+            return null;
+        }
+
+        public string GetCarsDescription(string url)
+        {
+            return null;
+        }
+    }
+}
--- ConsoleCarSelector/Program.cs
+++ ConsoleCarSelector/Program.cs
@@ -4,9 +4,13 @@ namespace ConsoleCarSelector
 {
     class Program
     {
+        private static string ROOT_URL = "ab.onliner.by";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var browser = new CarsRepositoryBrowser(
+                new CarRepository(ROOT_URL), 500);            
+            Console.WriteLine(browser.FindCarsByPriceAndDescription(1000, 3000, "USD", "техосмотр"));
         }
     }
 }
