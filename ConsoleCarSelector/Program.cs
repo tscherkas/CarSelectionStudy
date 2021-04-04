c93fb2ec9b37d961ce91960a8be48215f5bfb728
icharkas <icharkas@servicechannel.com>
2021-04-04 17:57:20 +0300

    Create console app.

---
+++ ConsoleCarSelector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCarSelector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
